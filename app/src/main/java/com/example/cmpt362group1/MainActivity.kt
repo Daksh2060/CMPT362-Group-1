60ce23f0a8558b12a9491b7d4ffeb19e6f38bded
Daksh Patel <dpa45@sfu.ca>
2025-10-30 15:04:32 -0700

    cleaned main / test commit

--- app/src/main/java/com/example/cmpt362group1/MainActivity.kt
+++ app/src/main/java/com/example/cmpt362group1/MainActivity.kt
@@ -16,32 +16,5 @@ import com.example.cmpt362group1.ui.theme.CMPT362Group1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CMPT362Group1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CMPT362Group1Theme {
-        Greeting("Android")
     }
 }
