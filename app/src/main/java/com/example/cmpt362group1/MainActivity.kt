8234d193634382816a36430057e4a38ef2a02452
Daksh Patel <dpa45@sfu.ca>
2025-11-05 07:16:30 -0800

    Merge conflict resolution

--- app/src/main/java/com/example/cmpt362group1/MainActivity.kt
+++ app/src/main/java/com/example/cmpt362group1/MainActivity.kt
@@ -4,19 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import com.example.cmpt362group1.event.CreateEvent
 import com.example.cmpt362group1.event.EventViewModel
-import com.example.cmpt362group1.event.Fab
-import com.example.cmpt362group1.navigation.BottomNavigationBar
-import com.example.cmpt362group1.navigation.explore.MapStateHolder
-import com.example.cmpt362group1.navigation.planner.PlannerHost
-import com.example.cmpt362group1.navigation.planner.PlannerScreen
-import com.example.cmpt362group1.navigation.profile.ProfileScreen
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,43 +24,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun MainScreen() {
-    var selectedTab by remember { mutableStateOf("Explore") }
-    var isCreatingEvent by remember { mutableStateOf(false) }
 
-    Scaffold(
-        bottomBar = {
-            BottomNavigationBar(
-                currentScreen = selectedTab,
-                onTabSelected = { selectedTab = it }
-            )
-        },
-        floatingActionButtonPosition = FabPosition.Start,
-        floatingActionButton = {
-            Fab(
-                label = "Create Event",
-                icon = Icons.Default.Add,
-                onClick = { isCreatingEvent = true },
-            )
-        }
-    ) { innerPadding ->
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-        ) {
-            when (selectedTab) {
-                "Explore" -> MapStateHolder()
-                "Planner" -> PlannerHost()
-                "Profile" -> ProfileScreen()
-            }
-        }
-    }
-
-    if (isCreatingEvent) {
-        CreateEvent(
-            onExit = { isCreatingEvent = false }
-        )
-    }
-}
 
