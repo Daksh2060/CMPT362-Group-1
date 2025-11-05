baab75b79691a54b66f8d99770aec25b4564e67a
Daksh Patel <dpa45@sfu.ca>
2025-11-05 06:54:16 -0800

    Test Merge Conflict resolution

--- app/src/main/java/com/example/cmpt362group1/MainActivity.kt
+++ app/src/main/java/com/example/cmpt362group1/MainActivity.kt
@@ -14,7 +14,6 @@ import com.example.cmpt362group1.event.EventViewModel
 import com.example.cmpt362group1.event.Fab
 import com.example.cmpt362group1.navigation.BottomNavigationBar
 import com.example.cmpt362group1.navigation.explore.MapStateHolder
-import com.example.cmpt362group1.navigation.planner.PlannerHost
 import com.example.cmpt362group1.navigation.planner.PlannerScreen
 import com.example.cmpt362group1.navigation.profile.ProfileScreen
 class MainActivity : ComponentActivity() {
@@ -32,44 +31,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun MainScreen() {
-    var selectedTab by remember { mutableStateOf("Explore") }
-    var isCreatingEvent by remember { mutableStateOf(false) }
-
-    Scaffold(
-        bottomBar = {
-            BottomNavigationBar(
-                currentScreen = selectedTab,
-                onTabSelected = { selectedTab = it }
-            )
-        },
-        floatingActionButtonPosition = FabPosition.Start,
-        floatingActionButton = {
-            Fab(
-                label = "Create Event",
-                icon = Icons.Default.Add,
-                onClick = { isCreatingEvent = true },
-            )
-        }
-    ) { innerPadding ->
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-        ) {
-            when (selectedTab) {
-                "Explore" -> MapStateHolder()
-                "Planner" -> PlannerHost()
-                "Profile" -> ProfileScreen()
-            }
-        }
-    }
-
-    if (isCreatingEvent) {
-        CreateEvent(
-            onExit = { isCreatingEvent = false }
-        )
-    }
-}
-
