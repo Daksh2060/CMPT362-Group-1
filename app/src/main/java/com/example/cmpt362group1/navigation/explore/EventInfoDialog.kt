c17ba6ae6261149cce4374c66d3838599ffc5e16
Daksh Patel <dpa45@sfu.ca>
2025-11-27 15:14:24 -0800

    Fixed box height issue

--- app/src/main/java/com/example/cmpt362group1/navigation/explore/EventInfoDialog.kt
+++ app/src/main/java/com/example/cmpt362group1/navigation/explore/EventInfoDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -60,7 +61,9 @@ fun EventInfoDialog(
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
+                .padding(16.dp)
+                .heightIn(max = 550.dp),
+
             shape = RoundedCornerShape(24.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.White
